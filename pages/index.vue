21ab3962f52c0335f8a132353edb3ccc0d15b9f7
吴忠恒 <2402252516@qq.com>
2021-10-19 00:48:42 +0800

    练练手

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,5 @@
 <template>
 	<view class="content">
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
 	</view>
 </template>
 
@@ -24,28 +21,7 @@
 
 <style lang="scss" >
 	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+		height:30vh;
+		background-color: $app_green;
 	}
 </style>
