5cfd70a2a12d682f2bac318153416cbbfe19018d
吴忠恒 <2402252516@qq.com>
2021-11-16 13:53:32 +0800

    将APPID改为发行版

--- App.vue
+++ App.vue
@@ -11,16 +11,16 @@
 	@font-face {
 		font-family: 'webfont';
 		font-display: swap;
-		src: url('//at.alicdn.com/t/webfont_p5jy8c03nda.eot');
+		src: url('https://at.alicdn.com/t/webfont_p5jy8c03nda.eot');
 		/* IE9*/
-		src: url('//at.alicdn.com/t/webfont_p5jy8c03nda.eot?#iefix') format('embedded-opentype'),
+		src: url('https://at.alicdn.com/t/webfont_p5jy8c03nda.eot?#iefix') format('embedded-opentype'),
 			/* IE6-IE8 */
-			url('//at.alicdn.com/t/webfont_p5jy8c03nda.woff2') format('woff2'),
-			url('//at.alicdn.com/t/webfont_p5jy8c03nda.woff') format('woff'),
+			url('https://at.alicdn.com/t/webfont_p5jy8c03nda.woff2') format('woff2'),
+			url('https://at.alicdn.com/t/webfont_p5jy8c03nda.woff') format('woff'),
 			/* chrome、firefox */
-			url('//at.alicdn.com/t/webfont_p5jy8c03nda.ttf') format('truetype'),
+			url('https://at.alicdn.com/t/webfont_p5jy8c03nda.ttf') format('truetype'),
 			/* chrome、firefox、opera、Safari, Android, iOS 4.2+*/
-			url('//at.alicdn.com/t/webfont_p5jy8c03nda.svg#Alibaba-PuHuiTi-Regular') format('svg');
+			url('https://at.alicdn.com/t/webfont_p5jy8c03nda.svg#Alibaba-PuHuiTi-Regular') format('svg');
 		/* iOS 4.1- */
 	}
 
